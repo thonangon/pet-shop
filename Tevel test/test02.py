0d07e62e91c2404ee4616a179ee0063c00b95303
thona ngon <thona ngon@example.com>
2023-09-23 20:10:21 +0700

    test02

---
+++ Tevel test/test02.py
@@ -0,0 +1,11 @@
+def number(array,value):
+    
+    for n in range(len(array)):
+        res=n
+    return res
+arr1= ['banana','coconut','mango']
+arr2=[0,2]
+result = {}
+for j in arr2:
+    result[j]=arr1[j]
+print(result)
